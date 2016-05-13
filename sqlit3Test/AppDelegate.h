59cca42bd1254f8c247daae85293cccaa5b7dcee
Unrealplace <liyang84681111819@qq.com>
2016-05-13 11:17:27 +0800

    Initial Commit

---
+++ sqlit3Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sqlit3Test
+//
+//  Created by LiYang on 16/5/13.
+//  Copyright © 2016年 LiYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sqlit3Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sqlit3Test
+//
+//  Created by LiYang on 16/5/13.
+//  Copyright © 2016年 LiYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
